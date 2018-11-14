5a9bac9522be0800af1ecb98502e98fffd2fbe23
Tomasz <tomekradecki@gmail.com>
2018-11-14 01:57:32 +0100

    Add required library to install

---
+++ main.py
@@ -0,0 +1,37 @@
+import urllib
+import re
+import json
+from urllib import request
+from bs4 import BeautifulSoup
+
+
+def get_data_from_url():
+    url = 'https://www.futbin.com/'
+    headers = {}
+    headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
+    req = urllib.request.Request(url, headers=headers)
+    res = urllib.request.urlopen(req)
+    text = res.read()
+    soup = BeautifulSoup(text, 'html.parser')
+    totw_raw = str(soup.find('div', attrs={'id': 'totw'}))
+    soup = BeautifulSoup(totw_raw, 'html.parser')
+    players = soup.find_all('a', attrs={'target': ''})
+    totw = {}
+    for idx, player in enumerate(players):
+        regex = '\/19\/player\/\d+\/(.*?)\"'
+        name = re.search(regex, str(player))
+        rating = player.find('div', attrs={'class': 'pcdisplay-rat'})
+        if name is not None:
+            player_idx = f"player_{idx}"
+            player_totw = {}
+            player_totw[player_idx] = {"name:": name.group(1), 
+                                       "rating": rating.text}
+            totw.update(player_totw)
+    with open("totw_11.json", "w+") as f:
+        f.write(json.dump(totw, f))
+    print(totw)
+
+if __name__ == '__main__':
+    get_data_from_url()
+
+# <a href="/19/player/17203/Manuel Schäffler" target="">
